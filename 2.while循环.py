ce695fde03cfc6b97195e8f06a6df0c5ee21aa9a
panluluy <pan_luluy@163.com>
2021-12-23 17:22:54 +0800

    深浅拷贝

---
+++ 2.while循环.py
@@ -0,0 +1,15 @@
+username = "pan"
+password = '123456'
+
+
+"""
+# 方式一
+while True:
+    inp_user = input("请输入你的用户名：")
+    inp_pass = input("请输入你的密码：")
+    if inp_user == username and inp_pass == password:
+        print("{x:-^20}".format(x="登录成功"))
+        break
+    else:
+        print("登录失败")
+"""
