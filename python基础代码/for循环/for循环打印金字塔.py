f749fae08e30d04ab4f5aa58dbf3bf61b404712a
panluluy <pan_luluy@163.com>
2021-12-23 22:31:48 +0800

    for循环打印金字塔

--- python基础代码/for循环/for循环打印金字塔.py
+++ python基础代码/for循环/for循环打印金字塔.py
@@ -1,7 +1,9 @@
-row = 6
+row = 10
 for i in range(1,row):
     for block in range(1,row-i):
         print(end=" ")
     for j in range(1,i+1):
         print("*",end="")
+    for k in range(1,i):
+        print("*", end="")
     print()
